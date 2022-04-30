aa4f3926b809d89ade3f46bf932c43f30fffdb89
daniels66 <danielsaputra814@gmail.com>
2022-04-30 14:08:30 +0700

    mengedit di test2.java

--- src/latihan/Test2.java
+++ src/latihan/Test2.java
@@ -3,5 +3,6 @@ package latihan;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("membuat test2.java");
+        System.out.println("coba git dan github2");
     }
 }
